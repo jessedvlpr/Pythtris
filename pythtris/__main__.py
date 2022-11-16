dc4fd88b6052ea1319e021da0ada36b5dd87e221
Jesse Groves <grovesjesse@gmail.com>
2022-11-16 13:55:48 -0500

    added half baked rotation

--- pythtris/__main__.py
+++ pythtris/__main__.py
@@ -12,10 +12,13 @@ pygame.display.set_caption("Pythtris")
 framerate = 24
 frame = 0
 
-shape = random.choice(list(constants.shapes.keys()))
-up_next = random.choice(list(constants.shapes.keys()))
-shape_width = len(constants.shapes[shape][0]) - 1
-shape_height = len(constants.shapes[shape]) - 1
+shape_key = random.choice(list(constants.shapes.keys()))
+up_next_key = random.choice(list(constants.shapes.keys()))
+
+shape = constants.shapes[shape_key]
+
+shape_width = len(shape[0]) - 1
+shape_height = len(shape) - 1
 
 board = interface.Board()
 
@@ -27,23 +30,23 @@ while running:
     time.sleep(1/framerate)
     frame += 1
     screen.fill(0)
-    interface.visualize_board(board, shape, up_next, 30, 5, 1.15)
-    if frame >= framerate/(2 + board.level):
+    interface.visualize_board(board, shape_key, up_next_key, 30, 5, 1.15)
+    if frame >= framerate/(10 + board.level):
         able = True
         if pos_y + shape_height < board.height - 1:
-            for i, n in enumerate(constants.shapes[shape]):
+            for i, n in enumerate(shape):
                 for j, m in enumerate(n):
                     if m:
                         board.spaces[pos_y + i][pos_x + j] = 0
             pos_y += 1
-            for i, n in enumerate(constants.shapes[shape]):
+            for i, n in enumerate(shape):
                 for j, m in enumerate(n):
                     if m:
                         if board.spaces[pos_y + i][pos_x + j] != 0:
                             able = False
                             break
                         board.spaces[pos_y + i][pos_x +
-                                                j] = constants.values[shape]
+                                                j] = constants.values[shape_key]
                 if not able:
                     break
         else:
@@ -70,11 +73,13 @@ while running:
             if line_count == 4:
                 board.score += 1200 * (board.level + 1)
 
-            shape = up_next
-            up_next = random.choice(list(constants.shapes.keys()))
+            shape_key = up_next_key
+            up_next_key = random.choice(list(constants.shapes.keys()))
 
-            shape_width = len(constants.shapes[shape][0]) - 1
-            shape_height = len(constants.shapes[shape]) - 1
+            shape = constants.shapes[shape_key]
+
+            shape_width = len(shape[0]) - 1
+            shape_height = len(shape) - 1
 
             pos_x = int(board.width/2) - 1
             pos_y = 0
@@ -85,29 +90,59 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 if pos_x <= 9 and pos_x > 0 and pos_y > 3 and board.spaces[pos_y][pos_x - 1] == 0:
-                    for i, n in enumerate(constants.shapes[shape]):
+                    for i, n in enumerate(shape):
                         for j, m in enumerate(n):
                             if m:
                                 board.spaces[pos_y + i][pos_x + j] = 0
                     pos_x -= 1
-                    for i, n in enumerate(constants.shapes[shape]):
+                    for i, n in enumerate(shape):
                         for j, m in enumerate(n):
                             if m:
                                 board.spaces[pos_y + i][pos_x +
-                                                        j] = constants.values[shape]
+                                                        j] = constants.values[shape_key]
 
             if event.key == pygame.K_RIGHT:
                 if pos_x + shape_width < 9 and pos_x >= 0 and pos_y > 3 and board.spaces[pos_y][pos_x + 1 + shape_width] == 0:
-                    for i, n in enumerate(constants.shapes[shape]):
+                    for i, n in enumerate(shape):
                         for j, m in enumerate(n):
                             if m:
                                 board.spaces[pos_y + i][pos_x + j] = 0
                     pos_x += 1
-                    for i, n in enumerate(constants.shapes[shape]):
+                    for i, n in enumerate(shape):
                         for j, m in enumerate(n):
                             if m:
                                 board.spaces[pos_y + i][pos_x +
-                                                        j] = constants.values[shape]
+                                                        j] = constants.values[shape_key]
+
+            if event.key == pygame.K_a:
+                for i, n in enumerate(shape):
+                    for j, m in enumerate(n):
+                        if m:
+                            board.spaces[pos_y + i][pos_x + j] = 0
+                temp = shape.copy()
+                shape.clear()
+                for i in range(len(temp[0])):
+                    shape.append([])
+                    for j in range(len(temp)):
+                        if temp[j][i]:
+                            shape[i].append(1)
+                        else:
+                            shape[i].append(0)
+                shape_width = len(shape[0]) - 1
+                shape_height = len(shape) - 1
+                for i, n in enumerate(shape):
+                    for j, m in enumerate(n):
+                        if m:
+                            board.spaces[pos_y + i][pos_x +
+                                                    j] = constants.values[shape_key]
+
+            if event.key == pygame.K_d:
+                temp = shape.copy()
+                for i, n in enumerate(shape):
+                    for j, m in enumerate(n):
+                        pass
+                shape_width = len(shape[0]) - 1
+                shape_height = len(shape) - 1
 
             if event.key == pygame.K_ESCAPE:
                 running = False
